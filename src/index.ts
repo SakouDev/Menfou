558d31d296cc51e5ac6da4220cc0b18a69852f09
SAKUTO-PORTABLE\LucVi <vigneronlucweb@gmail.com>
2022-11-07 13:33:05 +0100

    workflow[A

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
+app.get('/hello', (req, res) => res.send('Hello Menfou HAHA 1'));
 
-app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
+app.listen(3007, ()=> console.log("Application démarrée sur le port 3007"));
