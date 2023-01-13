edc4858f007e66b74df12606a45549270baf994a
SAKUTO-PORTABLE\LucVi <vigneronlucweb@gmail.com>
2023-01-13 13:57:56 +0100

    MERDE

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Menfou HAHA 1'));
+app.get('/hello', (req, res) => res.send(`EH SALUT ! ${process.env.MENFOU} !!!!!!`));
 
 app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
