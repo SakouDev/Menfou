8fbebd8a73de674b803c195e8af69a9b57d22002
SAKUTO-PORTABLE\LucVi <vigneronlucweb@gmail.com>
2023-01-13 14:41:27 +0100

    sombre fils de pute

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send(`EH SALUT ! ${process.env.MENFOU} !!!!!!`));
+app.get('/hello', (req, res) => res.send(`EH SALUT SOMBRE FILS DE PUTE <3! ${process.env.MENFOU} !!!!!!`));
 
 app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
